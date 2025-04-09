0ec5ef4c26dc52fd05d84d1374df8af759bdde27
Dishant Singh <122022552+DishantSinghDev2@users.noreply.github.com>
2025-04-09 17:52:54 +0000

    Fix debugMode setting in initClamAV function to disable debug logging

--- src/utils/clamav.js
+++ src/utils/clamav.js
@@ -5,7 +5,7 @@ export const initClamAV = async () => {
   const clamscan = await new NodeClam().init({
     removeInfected: false,
     quarantineInfected: false,
-    debugMode: true,
+    debugMode: false,
     clamdscan: {
       host: "127.0.0.1",
       port: 3310,
