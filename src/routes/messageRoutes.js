178bfb01fa0e85e2c28eba5efa5bb0470aab29c8
Dishant Singh <122022552+DishantSinghDev2@users.noreply.github.com>
2025-04-12 12:24:34 +0000

    fix: Correct reply route to use sendMessage instead of replyToMessage

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -18,7 +18,7 @@ router.post("/send", validateMessage, rateLimiter("sendMessage", 20, 60), sendMe
 router.get("/public-key/:userId", rateLimiter("getPublicKey", 30, 60), getUserPublicKey)
 router.get("/:userId/:recipientId", rateLimiter("getMessages", 30, 60), getMessages)
 router.post("/typing", rateLimiter("typing", 30, 10), sendTypingIndicator)
-router.post("/reply", validateMessage, rateLimiter("messageAction", 20, 60), replyToMessage)
+router.post("/reply", validateMessage, rateLimiter("messageAction", 20, 60), sendMessage)
 router.post("/react", rateLimiter("messageAction", 20, 60), addReaction)
 router.post("/delete", rateLimiter("messageAction", 10, 60), deleteMessage)
 router.post("/schedule", validateMessage, rateLimiter("messageAction", 10, 60), scheduleMessage)
